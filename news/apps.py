170963207a3e3c5127d9919438df6b71e79b228f
VIDZ <Vidaalbasha641@gmail.com>
2025-12-24 21:30:14 +0200

    Docstrin to apps.pys

--- news/apps.py
+++ news/apps.py
@@ -5,4 +5,13 @@ class NewsConfig(AppConfig):
     name = 'news'
 
     def ready(self) -> None:
+        """
+        Docstring for ready function when applications starts
+        
+        :param self: self
+        
+        :returns: None
+        
+        """
+
         import news.signals  # type: ignore
