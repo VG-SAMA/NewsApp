137d0219cbb23e568450fad824066ba7506953fa
VIDZ <Vidaalbasha641@gmail.com>
2025-12-28 10:29:10 +0200

    Made chnage as per reviewer feedback, thanks

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,11 @@ django.setup()
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc' 'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.todo',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
